b54e6a2da4ba9f81a962b02a752c5380fcd4ca3a
Aiswarya Regi <115168775+Aiswarya-Regi@users.noreply.github.com>
2023-03-08 18:54:34 +0530

    update banner.js

--- utility/banner.js
+++ utility/banner.js
@@ -4,21 +4,21 @@ const CONSTANTS = require('../config/constant');
  const banner =  [
 {
 id : 1,
-url : `https://blognodejsapp-vaibhavwadhwa.onrender.com/images/TropicalImage.jpg`,
+url : `https://node-blog-project.onrender.com/images/TropicalImage.jpg`,
 type : "Travel",
 date : "Janary 24, 2023",
 title : "Himachal Pradesh"
 },
 {
 id : 2,
-url : `https://blognodejsapp-vaibhavwadhwa.onrender.com/images/TropicalImage2.jpg`,
+url : `https://node-blog-project.onrender.com/images/TropicalImage2.jpg`,
 // type : "Travel",
 // date : "Janary 24, 2023",
 // title : "Manali"
 },
 {
 id : 3,
-url : `https://blognodejsapp-vaibhavwadhwa.onrender.com/images/TropicalImage3.jpg`,
+url : `https://node-blog-project.onrender.com/images/TropicalImage3.jpg`,
 // type : "Travel",
 // date : "Janary 24",
 // title : "Colorado"
